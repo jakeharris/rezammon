84c86549545c7bcb45e95a4730e3da31441c45c2
Jake Harris <milieu@javakat.io>
2015-05-21 14:11:00 -0500

    Play around with events and ids

--- index.js
+++ index.js
@@ -13,9 +13,39 @@ app.get('/', function (req, res) {
 
 io.on('connection', function(socket){
   'use strict';
-  socket.on('chat message', function (msg) {
-    console.log(msg)
-    io.emit('chat message', msg)
+  
+  var heroExists = false,
+      heroID,
+      numPlayers = 0,
+      numObservers = 0
+  
+  // IMPORTANT
+  // Always validate that whoever is claiming to be
+  // the hero has the correct id.
+  
+  if(!socket.id) {
+    socket.id = createHash('' + socket.handshake.address.address + new Date()) // should be some form of hash -- IP + join-timestamp checksum?
+    if(!heroExists) {
+      socket.isHero = true // this may turn into configuration for hero-related events
+      heroExists = true
+      heroID = socket.id
+      socket.emit('hero-connect')
+    } 
+    else numObservers++
+    numPlayers++
+    io.emit('user-connect', socket.id)
+    console.log('%%%%% NEW USER CONENCTED %%%%%')
+    console.log('id: ' + socket.id)
+    console.log('is hero? ' + socket.isHero)
+    console.log()
+    console.log('total number of players: ' + numPlayers)
+    console.log('total number of observers: ' + numObservers)
+    console.log()
+    console.log()
+  }
+  socket.on('hero-move', function (heroID, x, y) {
+    console.log(heroID + ' moves: (' + x + ', ' + y + ')')
+    io.emit('hero-move', x, y)
   })
 });
 
@@ -44,4 +74,9 @@ function normalizePort(val) {
   }
 
   return false;
+}
+
+/* Thanks to @lordvlad for this! http://stackoverflow.com/a/15710692/2774085 */
+function createHash(s){
+  return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
 }
