b628f4b3b828666da9f97cd8a43d8fc10bd27a3b
Jake Harris <milieu@javakat.io>
2015-05-20 20:01:35 -0500

    Trying to get heroku to work with express is such a pain...

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var app = require('express')(),
     http = require('http').Server(app),
     io = require('socket.io')(http),
-    port = 1107
+    port = normalizePort(process.env.PORT || '1107');
+
+
+app.set('port', port);
 
 app.get('/', function (req, res) {
   'use strict';
@@ -20,3 +23,25 @@ http.listen(port, function () {
   'use strict';
   console.log('listening on *:' + port)
 })
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  'use strict';
+  
+  var port = parseInt(val, 10)
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
