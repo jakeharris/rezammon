122db3ab82413d241817af2133355f3c61f8c252
Jake Harris <milieu@javakat.io>
2015-05-28 22:58:17 -0500

    Add basic tests for Player object

--- src/player.js
+++ src/player.js
@@ -1,9 +1,17 @@
-module.exports = Player
+var ParameterCountError = require('./errors').ParameterCountError
 
 function Player (id) {
+  if(id === undefined)
+    throw new ParameterCountError('Players must be given an ID.')
+  if(typeof id !== 'string')
+    throw new TypeError('Players\' IDs cannot be anything but strings.')
+    
+  
   this.id = id
 }
 
 Player.prototype.getID = function () {
   return this.id
 }
+
+module.exports = Player
---
+++ test/player.js
@@ -0,0 +1,21 @@
+var assert = require('assert'),
+    Player = require('../src/player'),
+    ParameterCountError = require('../src/errors').ParameterCountError
+
+describe('Player', function () {
+  it('should throw a ParameterCountError if no id is supplied at construction', function () {
+    assert.throws(function () {
+      new Player() 
+    }, ParameterCountError)
+  })
+  it('should not throw a TypeError if an id is supplied at construction', function () {
+    assert.doesNotThrow(function () {
+      new Player('0')
+    }, TypeError)
+  })
+  it('should have an id', function () {
+    var p = new Player('0')
+    assert.equal(typeof p, 'object')
+    assert.equal(typeof p.getID(), 'string')
+  })
+})
