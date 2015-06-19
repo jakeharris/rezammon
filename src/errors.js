a2fb691c0a29955dba8308a198b2ea8fca86b08b
Jake Harris <milieu@javakat.io>
2015-06-19 14:41:45 -0500

    Add stack traces to custom errors

--- src/errors.js
+++ src/errors.js
@@ -2,6 +2,7 @@
 function ParameterCountError(msg) {
   // The wrong number of parameters was supplied
   // to a function.
+  Error.captureStackTrace(this, this.constructor)
   this.name = 'ParameterCountError'
   this.message = msg
 }
@@ -10,6 +11,7 @@ function ConfiguredHeroError(msg) {
   // The hero was configured in a way we didn't
   // expect, or was configured when we expected
   // no hero to exist.
+  Error.captureStackTrace(this, this.constructor)
   this.name = 'ConfiguredHeroError'
   this.message = msg
 }
@@ -17,6 +19,7 @@ function ConfiguredHeroError(msg) {
 function MissingHeroError(msg) {
   // The hero was missing when we expected 
   // her to be there.
+  Error.captureStackTrace(this, this.constructor)
   this.name = 'MissingHeroError'
   this.message = msg
 }
@@ -25,6 +28,7 @@ function NotImplementedError(msg) {
   // The function wasn't complete, or an
   // interface method should have been overridden
   // that was not.
+  Error.captureStackTrace(this, this.constructor)
   this.name = 'NotImplementedError'
   this.message = msg
 }
@@ -33,6 +37,7 @@ function SocketNotConnectedError(msg) {
   // We expected a socket and got none,
   // or one that didn't have the functions
   // we needed.
+  Error.captureStackTrace(this, this.constructor)
   this.name = 'SocketNotConnectedError'
   this.message = msg
 }
