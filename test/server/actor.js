77f404c67caeee8b34369ad8dc334ea313bb7200
Jake Harris <milieu@javakat.io>
2015-07-15 23:10:50 -0500

    Red-light Actors' takeDamage function

--- test/server/actor.js
+++ test/server/actor.js
@@ -1,7 +1,8 @@
 var assert = require('assert'),
     Entity = require('../../src/server/entity'),
     Actor = require('../../src/server/actor'),
-    NotImplementedError = require('../../src/errors').NotImplementedError
+    NotImplementedError = require('../../src/errors').NotImplementedError,
+    ParameterCountError = require('../../src/errors').ParameterCountError
 
 describe('Actor', function () {
   context('constructor', function () {
@@ -20,6 +21,45 @@ describe('Actor', function () {
         var a = new Actor()
         a.move()
       }, NotImplementedError)
+    })
+  })
+  context('takeDamage()', function () {
+    it('throws a ParameterCountError if an amount isn\'t specified', function () {
+      assert.throws(function () {
+        var a = new Actor()
+        a.takeDamage()
+      }, ParameterCountError)
+    })
+    it('throws a TypeError if the amount specified isn\'t a number', function () {
+      assert.throws(function () {
+        var a = new Actor()
+        a.takeDamage('amt')
+      }, TypeError)
+    })
+    it('throws a SyntaxError if the amount specified isn\'t nonnegative', function () {
+      assert.throws(function () {
+        var a = new Actor()
+        a.takeDamage(-15)
+      }, SyntaxError)
+    })
+    it('decreases health as expected', function () {
+      var a = new Actor()
+      a.health = 10
+      a.takeDamage(3)
+      assert.equal(a.health, 7)
+    })
+    it('sets health to zero when the damage would cause negative health', function () {
+      var a = new Actor()
+      a.health = 10
+      a.takeDamage(12)
+      assert.equal(a.health, 0)
+    })
+    it('kills the actor if the health is zero', function () {
+      var a = new Actor()
+      a.health = 10
+      assert.notEqual(a.hasOwnProperty('dead'))
+      a.takeDamage(12)
+      assert(a.hasOwnProperty('dead'))
     })
   })
 })
